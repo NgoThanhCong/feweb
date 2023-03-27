2cced4e41506d7ec968357d631ab11f80d9ceca0
ngothanhcong2882 <80748950+ngothanhcong2882@users.noreply.github.com>
2023-03-27 15:32:15 +0700

    test route

--- src/App.js
+++ src/App.js
@@ -1,68 +1,19 @@
-import logo from './logo.svg';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
 import './style.css';
+import Login from './page/admin/Login'
 
 function App() {
   return (
-    <>
-      <title>Log in Form</title>
-      <form action="" method="post">
-        <div className="container">
-          <div className="body">
-            {/* Form Login */}
-            <div className="form-container">
-              <div className="form-header">
-                <h4>Login</h4>
-              </div>
-              <div className="form">
-                <input
-                  type="text"
-                  className="form-input"
-                  placeholder="Email or phone number"
-                  required="true"
-                />
-                <input
-                  type="password"
-                  className="form-input"
-                  placeholder="Password"
-                  required="true"
-                />
-                <div className="form-help">
-                  <div className="">
-                    <input type="checkbox" className="checkbox" id="checkbox_id" />
-                    <label className="remember-lbl" htmlFor="checkbox_id">
-                      Remember me
-                    </label>
-                  </div>
-                  <a className="help-link" href="/">
-                    Need help?
-                  </a>
-                </div>
-                <button className="form-button">Sign In</button>
-              </div>
-              <div className="form-other">
-                <a href="/" className="fb">
-                  <img
-                    className="fb-icon"
-                    src="https://assets.nflxext.com/ffe/siteui/login/images/FB-f-Logo__blue_57.png"
-                    alt=""
-                  />
-                  <span className="fb-text">Login with Facebook</span>
-                </a>
-                <div className="term">
-                  <p>
-                    This page is protected by Google reCAPTCHA to ensure you're not
-                    a bot.
-                  </p>
-                  <a href="">Learn more.</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="overlay" />
-        </div>
-      </form>
-    </>
+    <BrowserRouter>
+          
+                <Routes>
+                    <Route path='/' element={<Login />} />                    
+                </Routes>
+          
+        </BrowserRouter>
+  
   )
 }
 
