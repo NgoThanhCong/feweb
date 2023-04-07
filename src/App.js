250f06091076daa34054a8de59938406763c6854
PHoMi126 <caidinhmenh2612@gmail.com>
2023-04-08 00:03:01 +0700

    Thừa Login Route

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import ViewIdea from "./page/admin/ViewIdea";
 import Login from "./Login";
 import NavBar from "./NavBar";
 import QacIdeas from "./page/qac/ViewIdea";
+import Dashboard from "./page/qam/Dashboard";
 
 function App() {
   return (
@@ -20,13 +21,13 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/Header" element={<Header />} />
       <Route path="/NavBar" element={<NavBar />} />  
-      <Route path="/Login" element={<Login />} />
       <Route path="/CreateAccount" element={<CreateAccount />} />
       <Route path="/UserList" element={<UserList />} />
       <Route path="/ViewIdea" element={<ViewIdea />} />
       <Route path="/QamIdeas" element={<QamIdeas />} />
       <Route path="/CatEditor" element={<Cat />} />
       <Route path="/QacIdeas" element={<QacIdeas />} />
+      <Route path="/Dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
